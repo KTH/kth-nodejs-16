0b357551aa5152c9ce4a6d8f0f9ffaa77bff9bbc
Patric Jansson <paddy@kth.se>
2021-06-21 18:54:12 +0200

    Inital check in with unit tests

---
+++ Dockerfile
@@ -0,0 +1,26 @@
+FROM node:16-alpine
+  
+RUN apk --no-cache add yarn git bash && \
+    # npm uses SSH to get code from github.
+    # We use https instead to skip the manual ssh fingerprint acceptance.
+    git config --global url."https://github.com/".insteadOf git@github.com: && \
+    git config --global url."https://".insteadOf git:// && \
+    git config --global url."https://".insteadOf ssh:// && \
+    # Install missing deps
+    npm install --global merge-descriptors
+    
+RUN echo "Alpine version: `cat /etc/alpine-release `" >> KTH_OS
+RUN echo "Build date: `date`" >> KTH_OS
+
+RUN echo "IMAGE INFORMATION" > KTH_NODEJS
+RUN echo "Based on:  node:16-alpine " >> KTH_NODEJS
+RUN echo "Build date: `date`" >> KTH_NODEJS
+RUN echo "Node: `node --version`" >> KTH_NODEJS
+RUN echo "NPM: `npm --version`" >> KTH_NODEJS
+RUN echo "Yarn: `yarn --version`" >> KTH_NODEJS
+RUN echo "" >> KTH_NODEJS
+RUN echo "--- Default global packages ---" >> KTH_NODEJS
+RUN echo "`npm list -g --depth 0`" >> KTH_NODEJS
+
+RUN cat KTH_OS
+RUN cat KTH_NODEJS
---
+++ tests/unit-tests/unit-tests.sh
@@ -0,0 +1,67 @@
+#!/bin/bash
+
+info() { printf "\033[1;31m\n   %s\033[0;33m$@\n\n";  }
+error() { printf "\033[1;31m • $@\033[0;0m\n"; }
+passed() { printf "\033[0;32m • $@\033[0;0m\n"; }
+
+FAILED=""
+
+cat /KTH_NODEJS
+#
+# /KTH_NODEJS should contain a pattern or show infor
+#
+# Usage: expectToContain "Build date" "No build date pressent in /KTH_NODEJS"
+#
+expectToContain() {
+    FILE=$(cat /$1)
+    PATTERN="$2"
+    FAILURE_INFO="$3"
+    
+    if [[ "$FILE" == *"$PATTERN"* ]]; then
+        if [ ! -z "$FAILURE_INFO" ]; then
+            passed "$FAILURE_INFO."
+        else 
+            passed "/KTH_NODEJS contains $PATTERN"
+        fi
+ 
+    else
+        if [ ! -z "$FAILURE_INFO" ]; then
+            error "$FAILURE_INFO"
+        fi
+        info "/KTH_NODEJS does not contain pattern '$PATTERN'."
+        
+        FAILED="true"
+    fi
+
+}
+
+# ---------------- Tests ----------------
+
+echo ""
+echo "OS"
+expectToContain "/KTH_OS" "Build date:" "/KTH_OS should contain the date when the images was built."
+expectToContain "/KTH_OS" "Alpine version: 3" "/KTH_OS should contain the Alpine version."
+
+echo ""
+echo "Node JS"
+expectToContain "/KTH_NODEJS" "Build date:" "/KTH_NODEJS should contain the date when the images was built."
+expectToContain "/KTH_NODEJS" "Node: v16" "Image should have Node v16* installed."
+expectToContain "/KTH_NODEJS" "NPM: 7" "Image should have 'npm 7.*.*' installed."
+expectToContain "/KTH_NODEJS" "Yarn: 1." "Image should have 'Yarn 1.*.*' installed."
+expectToContain "/KTH_NODEJS" "merge-descriptors@" "Image should have global package 'merge-descriptors' installed."
+
+# Result
+if [[ "$FAILED" != *"true"* ]]; then
+    info "All end-to-end tests passed."
+    exit 0
+else
+    echo ""
+    exit 1
+fi
+
+
+
+
+
+
+
